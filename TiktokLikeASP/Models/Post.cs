f3cc8198849b622314acb4c2cfd98c1e99a99a1a
abdolm <sasanabdolm@gmail.com>
2023-07-10 14:07:59 +0200

    model refine

--- TiktokLikeASP/Models/Post.cs
+++ TiktokLikeASP/Models/Post.cs
@@ -2,5 +2,18 @@
 {
     public class Post
     {
+        public Guid id { get; set; }
+        public string title { get; set; }
+
+        public string video_link { get; set; }
+
+        public Boolean is_visible { get; set; }
+
+        public List<Tag> Tags { get; } = new();
+
+        public List<Users> Users { get; } = new();
+
+
+        public List<Comments> Comments { get; } = new();
     }
 }
